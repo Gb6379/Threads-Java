54dbcdc0e1db351dffe4dc4d2c60e00b78a78112
Gb6379 <gabriel.am.rosa@gmail.com>
2022-08-28 22:40:44 -0300

    questao 4

--- Trab4/Questao4.java
+++ Trab4/Questao4.java
@@ -1,8 +1,19 @@
 package Trab4;
+import javax.swing.JOptionPane;
+import java.util.Random;
 
 public class Questao4 {
 
     public static void main(String[] args){
+
+        int threadsN = Integer.parseInt(JOptionPane.showInputDialog("Type threads number"));
+        Random r = new Random();
+
+        for (int i = 0; i < threadsN; i++) {
+
+            ThreadCount t = new ThreadCount(r.nextInt(100000 ), r.nextInt(5000));
+            t.start();
+        }
         
     }
     
--- Trab4/ThreadCount.java
+++ Trab4/ThreadCount.java
@@ -2,8 +2,36 @@ package Trab4;
 
 public class ThreadCount extends Thread {
 
+    int number;
+    int sleepT;
+
+    public ThreadCount(int number, int sleepT) {
+        this.number = number;
+        this.sleepT = sleepT;
+    }
+
     public void run() {
         
+        System.out.println("Number: " + number  +" Thread: " + Thread.currentThread().getId() + " Criada");
+
+        try {
+
+            for (int i = 0; i < number; i++) {
+                System.out.println(i);
+            }
+            System.out.println("Thread " + Thread.currentThread().getId() + " Entrando em espera");
+        
+                Thread.sleep(sleepT);
+                System.out.println("Thread " + Thread.currentThread().getId() + " Saindo de espera");
+                System.out.println(" Execução Thread " + Thread.currentThread().getId() + " finalaizada");
+            
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+        
+     
+         
+    
     }
     
 }
