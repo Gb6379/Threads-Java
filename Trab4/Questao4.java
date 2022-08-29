4b73f9c19014b48e82d84a51db4a877491ed98c1
Gb6379 <gabriel.am.rosa@gmail.com>
2022-08-28 22:12:04 -0300

    new folder

---
+++ Trab4/Questao4.java
@@ -0,0 +1,9 @@
+package Trab4;
+
+public class Questao4 {
+
+    public static void main(String[] args){
+        
+    }
+    
+}
---
+++ Trab4/ThreadCount.java
@@ -0,0 +1,9 @@
+package Trab4;
+
+public class ThreadCount extends Thread {
+
+    public void run() {
+        
+    }
+    
+}
